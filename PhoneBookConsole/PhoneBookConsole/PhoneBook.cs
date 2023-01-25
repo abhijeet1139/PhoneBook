f0b76da69b9749c956f6095706a32d48fe637c71
Abhijeet Pawar <98808368+abhijeet1139@users.noreply.github.com>
2023-01-25 18:17:44 +0530

    Add files via upload

---
+++ PhoneBookConsole/PhoneBookConsole/PhoneBook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookConsole
+{
+    internal class PhoneBook
+    {
+        private List<Contact> _contacts { get; set; } = new List<Contact>();
+
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact :{contact.Name},{contact.Number} ");
+        }
+
+        private void DisplayContactDetails(List<Contact>contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+        public void AddContact( Contact contact)
+        {
+            _contacts.Add(contact);
+             
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+
+            if(contact == null)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+        
+        public void DisplayAllContact()
+        {
+            DisplayContactDetails(_contacts);
+        }
+
+        public void DisplatMatchingContacts(string sarchPhrase)
+        {
+
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(sarchPhrase)).ToList();
+
+            DisplayContactDetails(matchingContacts);
+        }
+    }
+}
---
+++ PhoneBookConsole/PhoneBookConsole/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From the Console PhoneBook App");
+            Console.WriteLine("Select Operation");
+            Console.WriteLine("1 Add Contact");
+            Console.WriteLine("2 Display Contact by Number");
+            Console.WriteLine("3 View All Contacts");
+            Console.WriteLine("4 Sarch Contacts for given Name");
+            Console.WriteLine("Press 'X to'Exait");
+
+            var userInput = Console.ReadLine();
+
+            var phoneBook = new PhoneBook();
+
+            while(true)
+            { 
+             switch (userInput)
+            {
+                case "1":
+                    Console.WriteLine("Contact Name");
+                    var name = Console.ReadLine();
+
+                    Console.WriteLine("Contact Number");
+                    var number = Console.ReadLine();
+
+                    var newContact = new Contact(name, number);
+                    phoneBook.AddContact(newContact);
+
+
+                    break;
+                case "2":
+                        Console.WriteLine("Contact Number to sarch : ");
+                        var searchNumber = Console.ReadLine();
+
+                        phoneBook.DisplayContact(searchNumber);
+
+                        break;
+                case "3":
+                    break;
+                case "4":
+                        Console.WriteLine("Name Sarch Phrase");
+                        var sarchPhrase = Console.ReadLine();
+
+                        phoneBook.DisplatMatchingContacts(sarchPhrase);
+
+                    break;
+                    case "x":
+                        return;
+                default:
+                    Console.WriteLine("Select Valid Operation");
+                    break;
+            }
+
+                Console.WriteLine("Select Operation");
+                userInput = Console.ReadLine();
+            }
+        
+        }
+    }
+}
